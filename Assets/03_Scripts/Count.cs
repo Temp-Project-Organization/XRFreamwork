527f3ab2b1fd92aa1718a6e1a71f907d92c63911
Gwang Chun, Seo <35924301+SeoRiel@users.noreply.github.com>
2022-12-16 04:15:26 +0900

    Update Count.cs

--- Assets/03_Scripts/Count.cs
+++ Assets/03_Scripts/Count.cs
@@ -8,14 +8,21 @@ public class Count : MonoBehaviour
 
     [SerializeField][Range(0.0f, 100.0f)] private float damp = 1.0f;
 
+    private int enemyCount;
+
     private void Awake()
     {
         gameManager = GameObject.Find("GameManager");
     }
 
+    private void Update()
+    {
+        enemyCount = (int)GameObject.FindGameObjectsWithTag("ENEMY").Length;
+    }
+
     private void FixedUpdate()
     {
-        if (gameManager.GetComponent<GameManager>().GetEnemyCount() == 0)
+        if (enemyCount == 0)
         {
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(new Vector3(100.0f, 0.0f, 0.0f)), Time.deltaTime * damp);
         }
